46221d09507db1c4738848fcf08f1bbaae12b4c5
= <=>
2023-03-28 19:49:56 +0200

    minitalk 3

--- CURSUS42/minitalk/client.c
+++ CURSUS42/minitalk/client.c
@@ -6,29 +6,64 @@
 /*   By: lucifern <lucifern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 13:33:27 by lucifern          #+#    #+#             */
-/*   Updated: 2023/03/25 19:50:09 by lucifern         ###   ########.fr       */
+/*   Updated: 2023/03/28 18:00:26 by lucifern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(int argc, char *argv[])
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void	send_signal(int pid, int sig)
+{
+	if (sig == 1)
+	{
+		printf("called: 1\n");
+		kill(pid, SIGUSR2);
+	}
+	else
+	{
+		printf("called: 0\n");
+		kill(pid, SIGUSR1);
+	}
+}
+
+void	char_to_bin(int pid, char c)
+{
+	int	n;
+	int	b;
+
+	n = c;
+	printf("c: %c is n: %d\n", c, n);
+	while (n != 0)
+	{
+		b = n % 2;
+		printf("b: %i\n", b);
+		send_signal(pid, b);
+		n = n / 2;
+		usleep(1);
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
 /*
 	El cliente debe comunicar la string pasada como parámetro al servidor.
 	Parámetros: PID del servidor y string a enviar.
 */
-	//establecer conexion con pid
-	//mensaje a binario
-	//ponerse a escuchar
-	//enviar mensaje
-	//recibir ok
-	//desbinariar ok
-	//imprimir ok
-	//fin
 {
-	int		pid;
-	char	*message;
+	int	i;
+	int	c;
+	int	pid;
 
-	pid = (int)argv[1];
-	if (pid < 1)
-	message = argv[argc];
+	i = 0;
+	pid = atoi(argv[1]);
+	while (argv[2][i])
+	{
+		c = argv[2][i];
+		char_to_bin(pid, c);
+		i++;
+	}
 	return (0);
 }
--- CURSUS42/minitalk/server.c
+++ CURSUS42/minitalk/server.c
@@ -6,33 +6,47 @@
 /*   By: lucifern <lucifern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 13:33:30 by lucifern          #+#    #+#             */
-/*   Updated: 2023/03/25 19:58:23 by lucifern         ###   ########.fr       */
+/*   Updated: 2023/03/28 17:55:59 by lucifern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <signal.h>
 #include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
 
+char	*g_num;
+
+void	handler_sigusr1(void)
+{
+	*g_num = '1';
+	g_num++;
+	printf("NUM1: %s\n", g_num - 1);
+}
+
+void	handler_sigusr2(void)
+{
+	*g_num = '0';
+	g_num++;
+	printf("NUM0: %s\n", g_num - 1);
+}
+
+int	main(void)
 /*
 	El servidor debe lanzarse primero, tras lanzarse debe mostrar su PID.
 	Una vez la string se haya recibido, el servidor debe mostrarla.
 	Parámetros: string que mostrar.
 */
-	//obtener pid
-	//imprmirlo
-	//while true
-		//sleep
-		//recibir señal
-		//desbinariar mensaje??
-		//imprimir mensaje
-		//decir ok en binario
-int	main(void)
 {
-	int	pid;
-
-	pid = getpid();
-	printf("%i", pid);
+	signal(SIGUSR1, handler_sigusr1);
+	signal(SIGUSR2, handler_sigusr2);
+	printf("pid: %i\n", getpid());
+	g_num = malloc(8);
+	while (1)
+	{
+		printf("HOLA: %s\n", g_num - 7);
+		sleep(1);
+	}
 	return (0);
 }
 
